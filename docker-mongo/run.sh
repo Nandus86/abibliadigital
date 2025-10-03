68497bc9522b04e31411d6b8656bc6c9e9d16d28
Nandus86 <164101455+Nandus86@users.noreply.github.com>
2025-10-03 11:52:30 -0300

    Update run.sh

--- docker-mongo/run.sh
+++ docker-mongo/run.sh
@@ -3,4 +3,15 @@ set -m
 
 mongod --config /etc/mongod.conf --bind_ip_all &
 
+if [ ! -f /data/db/mongod.lock ]; then
+  sleep 5
+  mongoimport --db abibliadigital --collection books --file /files/books.json --jsonArray
+  mongoimport --db abibliadigital --collection testament --file /files/testament.json --jsonArray
+  mongoimport --db abibliadigital --collection users --file /files/users.json --jsonArray
+  mongoimport --db abibliadigital --collection verses --type json --file /files/verses-aa.json --jsonArray
+  mongoimport --db abibliadigital --collection verses --type json --file /files/verses-acf.json --jsonArray
+  mongoimport --db abibliadigital --collection verses --type json --file /files/verses-nvi.json --jsonArray
+  mongoimport --db abibliadigital --collection versions --file /files/versions.json --jsonArray
+fi
+
 fg
